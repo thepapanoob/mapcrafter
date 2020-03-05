b497145fe1e4320f1a761a4579ca1eb1c462367d
Dinip <dinis_pimpao@hotmail.com>
2020-03-05 22:03:54 +0000

    added features from 1.15 branch

---
+++ render.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#Renders the markers and the map
+/usr/local/bin/mapcrafter_markers -c /config/render.conf >> /config/mapcrafter.log 2>&1 && \
+/usr/local/bin/mapcrafter -j $threads -c /config/render.conf >> /config/mapcrafter.log 2>&1 && \
+
+#Just to make sure that title and favicon are kept after the render is finished
+#Copy title and favicon to be used in index.html
+cp /config/title.txt /output/static/html/title.txt && \
+cp /config/favicon.png /output/static/html/favicon.png
